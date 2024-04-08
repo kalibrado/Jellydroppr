2b6710ae862e6a5da0901a89573953ff141ec938
Leonardo <leonardo.fonseca85300@gmail.com>
2024-04-08 16:10:30 +0000

    init refacto

---
+++ project/main.py
@@ -0,0 +1,12 @@
+# main.py
+
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
