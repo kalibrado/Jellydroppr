5e3e2b89f2d57923f0d7a47fdd480b6494d7c024
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-04-11 09:57:08 +0000

    add dotfiles

---
+++ dotfiles/install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+echo "Creating symlink to bash folder in home directory."
+cp -R ./bash/. $HOME/
+
+echo "Creating symlink to zsh folder in home directory."
+cp -R ./zsh/. $HOME/
+
+source  $HOME/.bashrc
+
+echo "update"
+sudo apt-get -qq update
+
+echo "install -y python3 python3-venv python3-pip"
+sudo apt-get -qq install -y python3 python3-venv python3-pip
+
+echo "Install ZSH"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "upgrade pip"
+pip install -q --upgrade pip
+
+# Visual Studio Code :: Package list
+pkglist=(
+    aaron-bond.better-comments
+    alexcvzz.vscode-sqlite
+    batisteo.vscode-django 
+    DavidAnson.vscode-markdownlint
+    dbaeumer.vscode-eslint
+    donjayamanne.githistory
+    DotJoshJohnson.xml
+    eamodio.gitlens
+    ecmel.vscode-html-css
+    esbenp.prettier-vscode
+    felipecaputo.git-project-manager
+    foxundermoon.shell-format 
+    GitHub.vscode-pull-request-github
+    GrapeCity.gc-excelviewer  
+    rogalmic.bash-debug 
+    mads-hartmann.bash-ide-vscode
+    mechatroner.rainbow-csv
+    KevinRose.vsc-python-indent
+    donjayamanne.python-environment-manager
+    donjayamanne.python-extension-pack
+    ms-python.black-formatter
+    ms-python.isort
+    ms-python.mypy-type-checker
+    ms-python.pylint
+    ms-python.python
+    ms-toolsai.jupyter-keymap
+    ms-vscode.cpptools-themes
+    ms-vscode.live-server
+    ms-vscode.test-adapter-converter
+    hbenl.vscode-test-explorer
+    redhat.vscode-yaml
+    timonwong.shellcheck
+    vscode-icons-team.vscode-icons
+    wholroyd.jinja
+    yzhang.markdown-all-in-one
+    Zignd.html-css-class-completion
+    ms-python.python
+    donjayamanne.githistory
+    eamodio.gitlens
+    github.github-vscode-theme
+    oderwat.indent-rainbow
+    christian-kohler.path-intellisense
+    mhutchie.git-graph
+    Gruntfuggly.todo-tree
+    github.vscode-pull-request-github
+    marp-team.marp-vscode
+    shuworks.vscode-table-formatter
+    njpwerner.autodocstring
+)
+
+if test /tmp/code-server/bin/code-server; then
+    for i in ${pkglist[@]}; do
+        /tmp/code-server/bin/code-server --install-extension $i
+    done
+fi
+
+echo "Add Settings code-server"
+cp ./settings.json  $HOME/.local/share/code-server/User
